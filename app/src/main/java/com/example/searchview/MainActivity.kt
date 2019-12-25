4697397855716265fa4d4e92ebf4c869aeb13704
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-25 20:48:04 +0700

    declare variables of searchview and listview

--- app/src/main/java/com/example/searchview/MainActivity.kt
+++ app/src/main/java/com/example/searchview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.searchview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var searchView: SearchView
+    lateinit var myList: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchView.findViewById<SearchView>(R.id.searchView)
+        myList.findViewById<ListView>(R.id.myList)
     }
 }
