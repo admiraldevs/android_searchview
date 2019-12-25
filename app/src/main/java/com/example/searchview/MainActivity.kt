1250c2712f4830ba578ecc95e654779b68e17238
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-25 20:53:40 +0700

    declare variable of arrayList and add some data to it

--- app/src/main/java/com/example/searchview/MainActivity.kt
+++ app/src/main/java/com/example/searchview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.searchview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.widget.SearchView
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var searchView: SearchView
     lateinit var myList: ListView
+    lateinit var list: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +18,20 @@ class MainActivity : AppCompatActivity() {
 
         searchView.findViewById<SearchView>(R.id.searchView)
         myList.findViewById<ListView>(R.id.myList)
+
+        addDataList()
+    }
+
+    private fun addDataList(): ArrayList<String>{
+        // add data
+        list.add("Monday")
+        list.add("Tuesday")
+        list.add("Wednesday")
+        list.add("Thursday")
+        list.add("Friday")
+        list.add("Saturday")
+        list.add("Sunday")
+
+        return list
     }
 }
